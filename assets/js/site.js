33346d7450c0f91633807f5888de8cd120489598
bo-nicolaisen <bo@squirrelworks.dk>
2024-09-04 13:45:37 +0200

    base

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,19 @@
-// write cool JS hwere!!
+// http response codes see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
+
+// #section
+function getData(myUrl){
+
+    fetch(myUrl)
+    .then((response)=>{
+        console.log(response);
+        return response.json();
+    })
+   .then((data)=>{
+        console.log(data)
+
+})
+}
+
+
+
+getData('https://dummyjson.com/http/429')
