0df144e13027320d372e85161f568fed5e16f4dc
bo-nicolaisen <bo@squirrelworks.dk>
2024-09-04 13:57:33 +0200

    base

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,32 @@
-// http response codes see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
 
-// #section
+//errorchecking with logic operator
+
+myFunction()
+
+function myFunction(myData){
+if (myData) {
+    console.log(myData);
+}else{
+console.log('no data sendt');
+}
+
+}
+
+
+//errorchecking with logic operator and data type
+myArrayFunction()
+
+function myArrayFunction(mydata){
+
+    mydata.forEach(element => {
+       console.log(element); 
+    });
+}
+
+
+
+// error checking in fetch
+// http response codes see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
 function getData(myUrl){
 
     fetch(myUrl)
@@ -10,10 +36,12 @@ function getData(myUrl){
     })
    .then((data)=>{
         console.log(data)
-
+})
+.catch((error)=>{
+    console.log('my error: '+error);
 })
 }
 
-
-
 getData('https://dummyjson.com/http/429')
+
+
