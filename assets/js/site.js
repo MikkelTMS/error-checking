fa5e3b4d19d3247518dff34aa1bf0ec4c1f7e961
MikkelTMS <mtamsgaard@gmail.com>
2024-09-05 10:25:56 +0200

    Fixet
    Fixet

--- assets/js/site.js
+++ assets/js/site.js
@@ -14,13 +14,16 @@ function myFunction(myData) {
 
 
 // denne funktion skal checkes for modtagelse af en array
-myArrayFunction()
+myArrayFunction(['element1', 'element2', 'element3']);
 
 function myArrayFunction(mydata) {
-
-    mydata.forEach(element => {
-        console.log(element);
-    });
+    if (mydata && Array.isArray(mydata)) {
+        mydata.forEach(element => {
+            console.log(element);
+        });
+    } else {
+        console.log('Det satme ikke en array!');
+    }
 }
 
 
@@ -29,23 +32,26 @@ function myArrayFunction(mydata) {
 // http response codes see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
 
 //her skal du checke om vi har et respons der er ok
-function getData(myUrl) {
-
-    fetch(myUrl)
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data)
-        })
 
 
-        .catch((error) => {
-            console.log('my error: ' + error);
-        })
-}
 
-getData('https://dummyjson.com/http/429')
+// function getData(myUrl) {
+
+//     fetch(myUrl)
+//         .then((response) => {
+//             console.log(response);
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data)
+//         })
+
+
+//         .catch((error) => {
+//             console.log('my error: ' + error);
+//         })
+// }
+
+// getData('https://dummyjson.com/http/429')
 
 
