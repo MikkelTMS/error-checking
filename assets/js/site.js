3dbfa666fbc5dfcc9268f005a38c3d350b49d87f
bo-nicolaisen <bni@techcollege.dk>
2024-09-05 08:33:44 +0200

    opgaver

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,10 @@
 
 //errorchecking with logic operator
 
+//Denne funktion skal checkes for om den modtager data
 myFunction()
 
-function myFunction(myData){
+function myFunction(myData) {
 
     console.log(myData);
 
@@ -11,13 +12,14 @@ function myFunction(myData){
 }
 
 
-//errorchecking with logic operator and data type
+
+// denne funktion skal checkes for modtagelse af en array
 myArrayFunction()
 
-function myArrayFunction(mydata){
+function myArrayFunction(mydata) {
 
     mydata.forEach(element => {
-       console.log(element); 
+        console.log(element);
     });
 }
 
@@ -25,19 +27,23 @@ function myArrayFunction(mydata){
 
 // error checking in fetch
 // http response codes see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
-function getData(myUrl){
+
+//her skal du checke om vi har et respons der er ok
+function getData(myUrl) {
 
     fetch(myUrl)
-    .then((response)=>{
-        console.log(response);
-        return response.json();
-    })
-   .then((data)=>{
-        console.log(data)
-})
-.catch((error)=>{
-    console.log('my error: '+error);
-})
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+
+
+        .catch((error) => {
+            console.log('my error: ' + error);
+        })
 }
 
 getData('https://dummyjson.com/http/429')
